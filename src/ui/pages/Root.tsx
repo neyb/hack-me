747790b93c38a7d2554a0b6f912ad49b14419e7c
neyb <noo.blaster@gmail.com>
2023-01-01 22:17:37 +0100

    add topbar

--- src/ui/pages/Root.tsx
+++ src/ui/pages/Root.tsx
@@ -1,3 +1,10 @@
+import { Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { TopBar } from "@/ui/common/layout/TopBar";
 
-export const Root = () => <div><div>root</div><Outlet/></div>
+export const Root = () => (
+  <Container>
+    <TopBar />
+    <Outlet />
+  </Container>
+);
