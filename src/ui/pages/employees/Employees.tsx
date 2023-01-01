3016ea61a34529512163e95d2a15d0e8b7f5f37a
neyb <noo.blaster@gmail.com>
2023-01-01 22:17:41 +0100

    update employess to show the list

--- src/ui/pages/employees/Employees.tsx
+++ src/ui/pages/employees/Employees.tsx
@@ -1,3 +1,52 @@
+import {
+  EmployeeComputed,
+  employeesSelector,
+} from "@/logic/infra/state/employeeSelectors";
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useRecoilValue } from "recoil";
+
+const EmployeeRow = ({ employee }: { employee: EmployeeComputed }) => (
+  <TableRow>
+    <TableCell>{employee.id}</TableCell>
+    <TableCell>{employee.name}</TableCell>
+    <TableCell>
+      {employee.preferedDesks.map((desk) => desk.label).join(", ")}
+    </TableCell>
+  </TableRow>
+);
+
+const Head = () => (
+  <TableHead>
+    <TableRow>
+      <TableCell>id</TableCell>
+      <TableCell>name</TableCell>
+      <TableCell>desks</TableCell>
+    </TableRow>
+  </TableHead>
+);
 export default function Employees() {
-  return <div>Employees</div>;
+  const employees = useRecoilValue(employeesSelector);
+
+  return (
+    <Box>
+      <TableContainer>
+        <Table>
+          <Head />
+          <TableBody>
+            {employees.map((employee) => (
+              <EmployeeRow employee={employee} key={employee.id} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
 }
