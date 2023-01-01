76327156272513567e868b0345030f5b41de8aef
neyb <noo.blaster@gmail.com>
2023-01-01 22:17:39 +0100

    recoil root

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { createRouter } from "./ui/router";
+import { RecoilRoot } from "recoil";
 
 const router = createRouter();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
   </React.StrictMode>
 );
